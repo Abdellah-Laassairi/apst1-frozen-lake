a51a528d2139af8ed8664bb325bab683ce018e26
Abdellah-laassairi <laassairi.abdellah@gmail.com>
2023-03-07 00:01:56 +0100

    Added comments and quick fix

--- tools/qlearning.py
+++ tools/qlearning.py
@@ -2,13 +2,13 @@
 import numpy as np
 from tqdm import *
 
-
-def q_train_greedy(env, alpha=0.9, gamma=0.95, max_epsilon=1, epsilon=0.01, max_n_steps=100, n_episodes=30000, weights="data/q_table.npy"):
+def q_train_greedy(env, alpha=0.99, gamma=0.8, epsilon=0.01, max_n_steps=100, n_episodes=30000, weights="data/q_table.npy"):
     """ Q–learning algorithm (epsilon-greedy)
     
     """
     num_actions = env.action_space.n
     num_states = env.observation_space.n
+    
     try :
         print("Loading Q-table saved weights...")
         q_table = np.load(weights)
@@ -20,8 +20,6 @@ def q_train_greedy(env, alpha=0.9, gamma=0.95, max_epsilon=1, epsilon=0.01, max_
 
     rewards = []
 
-    epsilon = max_epsilon
-
     print("Starting Q-learning algorithm...")
     for _ in trange(n_episodes):
         s = env.reset()[0]
@@ -31,19 +29,23 @@ def q_train_greedy(env, alpha=0.9, gamma=0.95, max_epsilon=1, epsilon=0.01, max_
             if U < epsilon:
                 a = env.action_space.sample() # selecting action a at random from A 
             else:
-                a = np.argmax(q_table[s]) # Select action a given X following policy derived from q;
+                a = np.argmax(q_table[s]) # Select action a given s (state) following policy derived from q;
             
             s_new, r, done, _ , _= env.step(a)
             
+            # Update Q-table
             q_table[s, a] = (1-alpha)*q_table[s, a] + alpha*(r + gamma*np.max(q_table[s_new]))
             
+            # update state and total reward
             s, total_reward = s_new, total_reward+r
 
-            # if X is a terminal state then go to next episode;
+            # if s is a terminal state then go to next episode;
             if done: 
                 rewards.append(total_reward) 
                 break
+            
     env.close()
     print("Finished Q-learning algorithm.")
-    print("Average reward: {}".format(np.mean(rewards)))
+    print("Average reward is : {}".format(np.mean(rewards)))
+
     return q_table
