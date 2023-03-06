c66d8e5b1c7eae8cf9ae2f37903525757b1e4bb6
Abdellah-laassairi <laassairi.abdellah@gmail.com>
2023-03-06 15:23:59 +0100

    Quick fix to pygame not closing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import gymnasium as gym
 import pygame
-
+import sys
 
 def main():
     """
@@ -9,15 +9,20 @@ def main():
     """
     env = gym.make("FrozenLake-v1", render_mode="human")
     observation, info = env.reset(seed=42)
-    
-    for _ in range(50):
+
+    while True:
+        keys = pygame.key.get_pressed()
+
+
         action = env.action_space.sample()  # this is where you would insert your policy
         observation, reward, terminated, truncated, info = env.step(action)
 
-    if terminated or truncated:
-        observation, info = env.reset()
+        if terminated or truncated:
+            observation, info = env.reset()
+        env.reset()
+        if keys[pygame.K_ESCAPE]:
+            env.close()
 
-
-
+            
 if __name__ == '__main__':
     main()
