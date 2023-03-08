df43cb6305d672eb91cddf653f6770c6fa33e2a2
Romane <romane.teze@gmail.com>
2023-03-08 09:07:37 +0100

    implementation sarsa

--- tools/sarsa.py
+++ tools/sarsa.py
@@ -1,5 +1,37 @@
+import numpy as np
+from tqdm import *
 
-
-
-def sarsa_train(env):
-    return 0
+def sarsa_train(env, alpha, gamma, epsilon, q_init, episodes, steps) :
+    
+    q = q_init
+    
+    for ep in trange(episodes) : 
+        
+        env.reset()
+        state = env.s
+        
+        if np.random.uniform() < epsilon :
+            action = env.action_space.sample()
+        else :
+            action = np.argmax(q[state, :])
+            
+        for _ in range(steps) :
+            nw_state, reward, terminated, truncated, info = env.step(action)
+            
+            if np.random.uniform() < epsilon :
+                nw_action = env.action_space.sample()
+            else :
+                nw_action = np.argmax(q[nw_state, :])
+                
+            q[state, action] = (1-alpha)*q[state, action] + \
+                alpha*(reward + gamma*q[nw_state, nw_action])
+                
+            state = nw_state
+            action = nw_action
+            
+            if terminated or truncated :
+                break
+    
+    env.close()
+    
+    return q
